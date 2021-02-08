33c62793b2ea9bf37eeac2c16ab4d21c46c2a45a
Kenni Bawden <kenni@getup.org.au>
2021-02-09 10:35:56 +1100

    Stop adding duplicate numbers

--- app/models/identity_tijuana/user.rb
+++ app/models/identity_tijuana/user.rb
@@ -56,8 +56,10 @@ module IdentityTijuana
         })
       end
 
-      member_hash[:phones].push(phone: PhoneNumber.standardise_phone_number(home_number)) if home_number.present?
-      member_hash[:phones].push(phone: PhoneNumber.standardise_phone_number(mobile_number)) if mobile_number.present?
+      standard_home = PhoneNumber.standardise_phone_number(home_number) if home_number.present?
+      standard_mobile = PhoneNumber.standardise_phone_number(mobile_number) if mobile_number.present?
+      member_hash[:phones].push(phone: standard_home) if home_number.present?
+      member_hash[:phones].push(phone: standard_mobile) if mobile_number.present? and standard_mobile != standard_home
 
       UpsertMember.call(
         member_hash,
